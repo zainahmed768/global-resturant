a979ab6843ff34eee2a6a48ac12099271a1fc0b2
MShariqkhan12 <99810849+MShariqkhan12@users.noreply.github.com>
2022-08-09 18:27:54 +0500

    done

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php include 'inc/app.php'; ?>
+<link rel="stylesheet" href="css/contact.css">
+
+
+<!-- Contact section  -->
+<section class="contact-sec">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="contact-all">
+                    <button type="button" class="btn btn-primary" data-toggle="modal"
+                        data-target=".bd-example-modal-lg">Large modal</button>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End of Contact secton  -->
+<div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            ...
+        </div>
+    </div>
+</div>
+
+
+<?php include 'inc/footer.php'; ?>
