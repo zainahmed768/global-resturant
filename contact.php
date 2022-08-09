ab341820a37be555cac35e23a736df1fa110b79f
MShariqkhan12 <99810849+MShariqkhan12@users.noreply.github.com>
2022-08-09 19:25:11 +0500

    done

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@
             <div class="col-md-12">
                 <div class="contact-all">
                     <button type="button" class="btn btn-primary" data-toggle="modal"
-                        data-target=".bd-example-modal-lg">Large modal</button>
+                        data-target=".bd-example-modal-lg">Contact</button>
 
 
                 </div>
@@ -22,7 +22,79 @@
     aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            ...
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="contact-sec-all">
+                        <div class="close-icons">
+                            <i class="fa fa-times" aria-hidden="true"></i>
+
+                        </div>
+                        <div class="contact-head">
+                            <h3>CONTACT US</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                                ut <br> labore et dolore magna aliqua. Augue neque gravida in fermentum et sollicitudin
+                                ac.
+                                Magna ac.</p>
+
+
+                            <form action="">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+
+                                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                                aria-describedby="emailHelp" placeholder="First Name">
+
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+
+                                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                                aria-describedby="emailHelp" placeholder="Last Name">
+
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+
+                                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                                aria-describedby="emailHelp" placeholder="Email ">
+
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+
+                                            <input type="email" class="form-control" id="exampleInputEmail1"
+                                                aria-describedby="emailHelp" placeholder="Phone Number">
+
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+
+                                            <textarea class="form-control" id="exampleFormControlTextarea1"
+                                                placeholder="Comment Here..." rows="3"></textarea>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <div class="form-contact-submit-btn">
+                                            <button> Submit</button>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
